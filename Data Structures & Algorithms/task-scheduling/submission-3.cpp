e8a9b92232afb84a446f6593e7152148029d08a0
Girish9777 <mrgirishbansal9777@gmail.com>
2026-07-26 15:08:28 +0530

    Add: task-scheduling - submission-3

---
+++ Data Structures & Algorithms/task-scheduling/submission-3.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        priority_queue<int> pq;
+        vector<int> temp(26);
+        for(int i=0;i<tasks.size();i++){
+          temp[tasks[i]-'A']++;
+        }
+        for(int i=0;i<26;i++){
+          if(temp[i]>0){
+            pq.push(temp[i]);
+          }
+        }int time=0;
+        while(!pq.empty()){
+          vector<int> cool;
+          for(int i=1;i<=n+1;i++){
+            if(!pq.empty()){
+              int freq=pq.top();
+              freq--;
+              cool.push_back(freq);
+              pq.pop();
+            }
+          } 
+          for(int b:cool){
+            if(b>0)pq.push(b);
+          }
+          if(pq.empty()){
+            time+=cool.size();
+          }else{
+            time+=n+1;
+          }
+
+        }return time;
+
+    }
+};
