0cdf6db41c5df0252ed294ce11a87b45fb47aa07
Girish9777 <mrgirishbansal9777@gmail.com>
2026-08-02 01:59:46 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<vector<int>> output;
+        vector<int> ans;
+        rec(nums,output,ans,target,0,0);
+        return output;
+    }
+    void rec(vector<int>& nums,vector<vector<int>>& output,vector<int>& ans,int target,int idx,int sum){
+         // Pehle target check karna zaroori hai
+        if (sum == target) {
+            output.push_back(ans);
+            return;
+        }
+
+        // Uske baad index boundary
+        if (idx == nums.size()) {
+            return;
+        }
+
+        // choice 
+        if(check(idx,sum,target,nums)){
+            ans.push_back(nums[idx]);
+            rec(nums,output,ans,target,idx,sum+nums[idx]);
+            ans.pop_back();
+        }
+        rec(nums,output,ans,target,idx+1,sum);
+    }
+    bool check(int index,int sum,int target,vector<int>& nums){
+        if(nums[index]+sum<=target)return true;
+
+
+        else return false;
+    }
+};
