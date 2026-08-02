24fe945e17c502663c32e08b186a0a673e360d44
Girish9777 <mrgirishbansal9777@gmail.com>
2026-08-02 15:19:35 +0530

    Add: search-for-word - submission-0

---
+++ Data Structures & Algorithms/search-for-word/submission-0.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+       for(int row=0;row<board.size();row++){
+        for(int col=0;col<board[0].size();col++){
+            if(rec(board,word,row,col,0)){
+                return true;
+            }
+        }
+       }  return false;
+    }
+    bool rec(vector<vector<char>>& board,string word,int row,int col,int idx){
+        if(idx==word.size()){
+            return true;
+        }
+         // ERROR FIX: boundary check
+        if (row < 0 || col < 0 ||
+            row >= board.size() ||
+            col >= board[0].size()) {
+            return false;
+        }
+        //choice to not take
+        if(!check(board,word,row,col,idx)){
+            return false;
+        }
+
+        // it means we can taje this index and then we check the another level
+            char ch=board[row][col];
+            board[row][col]='*';
+            bool found=rec(board,word,row,col+1,idx+1)||rec(board,word,row,col-1,idx+1)||
+            rec(board,word,row-1,col,idx+1) ||rec(board,word,row+1,col,idx+1);
+            board[row][col]=ch;
+            return found ;
+        
+        
+    
+    }
+    bool check(vector<vector<char>>& board,string word,int row,int col,int idx)
+    {
+        if(board[row][col]!=word[idx]){
+            return false;
+        }
+        return true;
+    }
+};
